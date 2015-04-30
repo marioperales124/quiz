30dfbf675829f20537cc6f3e74a0cccd19398ba0
falbuisech <f.galbuisech@gmail.com>
2015-04-30 13:12:02 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('./routes/index');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+/* GET pagina preguntas*/
+router.get('/quizes/question', quizController.question);
+
+/* GET pagina respuestas */
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
