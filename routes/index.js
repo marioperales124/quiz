3d8d9b9197a7e59576bc3682f3936ad0ae1f3db8
falbuisech <f.galbuisech@gmail.com>
2015-05-01 21:18:24 +0200

    Marco de la aplicación

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('./routes/index');
+var quizController = require('../controllers/quiz_controllers.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
