f155c1c6cd05fb7bb78858859021f52c7a2e603f
marioperales124 <marioperales124@gmail.com>
2015-05-17 11:47:39 -0700

    Sesion

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controllers.js');
 var commentController = require('../controllers/comment_controller.js');
+var sessionController = require('../controllers/session_controller.js');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []})
 });
 
 router.param('quizId', quizController.load);
+
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
 
 /* GET pagina preguntas*/
 router.get('/quizes', quizController.index);
