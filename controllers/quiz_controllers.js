ddd6819719b52161ff3452793c9b47a21b7a8e9f
marioperales124 <marioperales124@gmail.com>
2015-05-17 05:09:23 -0700

    Borrar preguntas

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -90,3 +90,8 @@ exports.update = function(req, res){
       }
     })
 };
+exports.destroy = function(req, res){
+  req.quiz.destroy().then(function(req,res){
+      res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
