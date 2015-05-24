581f20c618c9a229ae8d3fd8befeb36b8efbc2f0
marioperales124 <marioperales124@gmail.com>
2015-05-23 19:37:00 -0700

    Busqueda de mis preguntas

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -28,15 +28,21 @@ exports.load= function(req, res, next, quizId){
 
 
 exports.index = function(req, res) {
+  
+    var options = {};
+ if(req.user){
+   options.where= {UserId: req.user.id};
+   options.order= 'pregunta ASC';
+ }
   if(req.query.search){
     models.Quiz.findAll({where: ["pregunta like ?",
     "%"+req.query.search.replace(" ","%")+"%"]}).then(function(quizes) {
       res.render('quizes/index.ejs', { quizes: quizes, errors: []});
     }).catch(function(error) { next(error);});
   }else{
-    models.Quiz.findAll().then(function(quizes) {
+    models.Quiz.findAll(options).then(function(quizes) {
       res.render('quizes/index.ejs', { quizes: quizes, errors: []});
-    }).catch(function(error) { next(error)});
+    }).catch(function(error) { next(error);});
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ router.get('/user/:userId(\\d+)/edit', sessionController.loginRequired, userCont
 router.put('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.update);
 router.delete('/user/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.destroy);
 
+router.get('/user/:userId(\\d+)/quizes', quizController.index);
 
 /* GET pagina preguntas*/
 router.get('/quizes', quizController.index);
